cd949c50dae3ee4c9b537b00b072ff9c89e3cd3e
Bentley <bentleygtr69@yahoo.com>
2018-06-25 20:54:07 -0500

    Food API started, adding ID's to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,23 @@
-//this is a test 
+$(document).ready(function(){
+    
+    
+    
+    
+  $("#submit").on("click", function(){  
+    var food = document.getElementById("num1").value;
+    var apikey = "637cf9cc5a93de2763c8c4a918f292a1";
+    var queryURL = "http://food2fork.com/api/search?key=" + apikey + "&q=" + food;
+    console.log(food);
+
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var results = response.data;
+
+    })
+  });  
+
+})
