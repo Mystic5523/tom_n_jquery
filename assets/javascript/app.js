7b2bea68656dae675e1205706ef3fe1c56924254
Mystic5523 <Mystic5523@hotmail.com>
2018-06-27 20:15:05 -0500

    iframe works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 
-$(document).ready(function(){
-    
-  jQuery.ajaxPrefilter(function(options) {
+$(document).ready(function () {
+
+  jQuery.ajaxPrefilter(function (options) {
     if (options.crossDomain && jQuery.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
     }
-});
-    
-    
-  $("#submit").on("click", function(){  
+  });
+
+
+  $("#submit").on("click", function () {
     var food1 = document.getElementById("num1").value;
     var food2 = document.getElementById("num2").value;
     var food3 = document.getElementById("num3").value;
@@ -22,70 +22,64 @@ $(document).ready(function(){
 
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        var results = JSON.parse(response).recipes;
-        console.log(JSON.parse(response));
-        for (i = 0; i < 4 ; i++) {
-         var foodImg = results[i].image_url;
-         console.log(foodImg);
-         
-         var actualImage = $("<img>")
-         actualImage.attr("src", foodImg);
-         $("#card").append(actualImage);
-        }
-        
-        // var actualImage = $("<img>")
-        // actualImage.attr("src", foodImg);
-        // $("#card").append(actualImage);
-        
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      var results = JSON.parse(response).recipes;
+      console.log(JSON.parse(response));
+      for (i = 0; i < 4; i++) {
+        var foodImg = results[i].image_url;
+        console.log(foodImg);
+
+        var actualImage = $("<img>")
+        actualImage.attr("src", foodImg);
+        $("#card").append(actualImage);
+      }
     });
-    
-  
-  
-  //this is a test 
+    // var actualImage = $("<img>")
+    // actualImage.attr("src", foodImg);
+    // $("#card").append(actualImage);
 
- var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      var recipeVid = 'M7lc1UVf-VE'
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: recipeVid,
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();}
-      })
-
+  });
 
 });
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
