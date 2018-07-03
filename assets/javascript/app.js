e3efcca69bbf681b043d1228ec08c46203a9724d
Bentley <bentleygtr69@yahoo.com>
2018-07-02 21:41:05 -0500

    validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,9 +32,16 @@ function submitHandler() {
     var food3 = document.getElementById("num3").value;
     var food4 = document.getElementById("num4").value;
     var food5 = document.getElementById("num5").value;
-    
+     
+    var patt = new RegExp("/^[a-zA-Z]+$/");
+    var res = patt.test(food1);
 
-    var apikey = "637cf9cc5a93de2763c8c4a918f292a1";
+    if (!/^[a-zA-Z]+$/.test(food1)){
+      $("#cardDeck").html("Not letters! Try again!");
+    }
+    else {
+      $("#cardDeck").empty();
+      var apikey = "637cf9cc5a93de2763c8c4a918f292a1";
     var queryURL = "https://food2fork.com/api/search?key=" + apikey + "&q=" + food1 + "," + food2 + "," + food3 + "," + food4 + "," + food5;
     console.log(food2);
     console.log(queryURL);
@@ -72,6 +79,46 @@ function submitHandler() {
         ;
       }
     });
+
+    }
+    // var apikey = "637cf9cc5a93de2763c8c4a918f292a1";
+    // var queryURL = "https://food2fork.com/api/search?key=" + apikey + "&q=" + food1 + "," + food2 + "," + food3 + "," + food4 + "," + food5;
+    // console.log(food2);
+    // console.log(queryURL);
+    // console.log(num1);
+
+
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET"
+    // }).then(function (response) {
+    //   var results = JSON.parse(response).recipes;
+    //   console.log(JSON.parse(response));
+    //   for (i = 0; i < 4; i++) {
+    //     var foodImg = results[i].image_url;
+    //     var foodLink = results[i].source_url;
+    //     var title = results[i].title;
+    //     console.log(foodImg);
+
+    //     var actualImage = $("<img>");
+    //     actualImage.attr({class: "card-img-top", "src": foodImg, width: "250", height: "200", id: "img" + (i+1)});
+    //     var imgLink = $("<a>");
+    //     imgLink.attr({"href": foodLink, target: "_blank"});
+
+    //     var titleLink = $("<h5 class='card-title'>" + title + "</h5>");
+    //     var caption =$("<div>");
+    //     caption.attr({class: "caption"});
+    //     console.log(title);
+    //     console.log (titleLink);
+    //     var cardBody = $("<div class='card-body'>")
+    //     var card = $("<div class='card'>");
+    //     card.append( actualImage, cardBody, caption, titleLink);
+    //     $("#cardDeck").append(card);
+
+    //     $("#img" + (i+1)).wrap(imgLink);
+    //     ;
+    //   }
+    // });
 }
 
 var tag = document.createElement('script');
